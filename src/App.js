128c78187b3da10a836b404999d07cfe440e4283
ieunune <50124623+ieunune@users.noreply.github.com>
2019-10-15 12:24:59 +0900

    20191015 Create

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{Component}from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+render(){
+  return(
+    <div classNmae="gray-background">
+      <img src={logo} lat="logo"></img>
+      <h2>Let's go</h2>
+    </div>
+  );
+}
+}
+
+export default App;

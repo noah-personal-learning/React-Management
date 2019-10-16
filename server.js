63241b53c8d9a6743714bcd9f61f12a9c90738ea
ieunune <50124623+ieunune@users.noreply.github.com>
2019-10-16 16:04:42 +0900

    20191016 1604 MYSQL Connect

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app=express();
@@ -6,37 +7,30 @@ const port  = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host : conf.host,
+    user: conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
+});
+
+connection.connect();
 // app.get('/api/hello', (req, res) => {
 //     res.send({message : 'Hello Server Excpress'});
 // });
 
 app.get('/api/customers', (req,res) => {
-    res.send([
-        {
-            'id' : 1,
-            'image' : 'http://placeimg.com/64/64/1',
-            'name' : '홍길동1',
-            'birthday' : '960720',
-            'gender' : '남자',
-            'job' : '학생'
-        },
-        {
-            'id' : 2,
-            'image' : 'http://placeimg.com/64/64/2',
-            'name' : '홍길동2',
-            'birthday' : '960720',
-            'gender' : '남자',
-            'job' : '프로그래머'
-        },
-        {
-            'id' : 3,
-            'image' : 'http://placeimg.com/64/64/3',
-            'name' : '홍길동3',
-            'birthday' : '960720',
-            'gender' : '남자',
-            'job' : '디자이너'
+    connection.query(
+        "SELECT * FROM customer",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-        ]);
+    )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

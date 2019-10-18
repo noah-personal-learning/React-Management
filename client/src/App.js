03ece96dca7c2788fafea82e669154c7779977af
ieunune <50124623+ieunune@users.noreply.github.com>
2019-10-18 20:53:54 +0900

    20191018 20:53 Add AppBar, WebFont

--- client/src/App.js
+++ client/src/App.js
@@ -10,25 +10,93 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 
 // rolding UI
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+// AppBar UI
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+
 const styles = theme => ({
   root : {
     width : '100%',
-    marginTop : theme.spacing.unit * 3,
-    overflowX : "auto"
-  },
-  table : {
     minWidth : 1080
+  },
+  paper : {
+    marginLeft : 15,
+    marginRight : 15
+  },
+  menu : {
+    marginTop : 15,
+    marginBottom : 15,
+    display : 'flex',
+    justifyContent : 'center'
+
   },
   progress : {
     margin : theme.spacing.unit * 2
   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+  tableHead : {
+    fontSize : "1.0rem",
+    color : "grey"
+  }
 })
 
 // 하드코딩 제거
@@ -110,19 +178,50 @@ class App extends Component {
   }
 render(){
   const { classes } = this.props;
+  const cellList = ["번호", "이미지", "이름", "생년월일", "성별", "직업", "설정"];
   return(
-    <div>
-      <Paper className={classes.root}>
+    <div  className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            고객관리시스템
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="검색"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      
+      <div className={classes.menu}>
+      <CustomerAdd stateRefresh={this.stateRefresh}/>
+      </div>
+
+      <Paper className={classes.paper}>
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-              <TableCell>설정</TableCell>
+              {cellList.map(c => {
+                return <TableCell className={classes.tableHead}>{c}</TableCell>
+              })}
+              
             </TableRow>
           </TableHead>
           <TableBody>
@@ -145,7 +244,7 @@ render(){
         <Customer id={customers[2].id} image={customers[2].image} name={customers[2].name} birthday={customers[2].birthday} gender={customers[2].gender} job={customers[2].job}/> */}
       
       </Paper>
-      <CustomerAdd stateRefresh={this.stateRefresh}/>
+      
     </div>
   );
 }
